8e07b45b727cd48e36dfbfe4728c9335a22ad8c7
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-09-16 09:24:14 +0530

    Module Based

--- Assignment/file.js
+++ Assignment/file.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 fs.readFile('emp.json','utf-8',(err,data)=>{
     if(err) throw err
-    let users=json.parse(data)
+    let users=JSON.parse(data)
     let male_users=[]
     let female_users=[]
-    for (user of users){
+    for (let user of users){
         if(user.gender=='male')
         {
              male_users.Push(user)
@@ -13,8 +13,8 @@ fs.readFile('emp.json','utf-8',(err,data)=>{
         {
             female_users.Push(user)
         }
-                        }})
+                        }
 
 fs.writeFile('male.json',JSON.stringify(male_users),(err)=>{if(err) throw err; console.log("successful")})
 fs.writeFile('female.json',JSON.stringify(female_users),(err)=>{if(err) throw err; console.log("successful")})
-
+})
---
+++ web/script.js
@@ -0,0 +1,8 @@
+import http from "http"
+import fs from "fs"
+import path from "path"
+
+let server=http.createServer((req,resp)=>{
+
+})
+server.listen(8080,'127.0.0.1',(err)=>{})
